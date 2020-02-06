ffd5fc135665450873abafa24f837c358385f3f5
Tanner York <tannerwyork@gmail.com>
2020-02-05 16:07:29 -0800

    Add report form image capture

--- screens/ReportScreens/ReportPhotoScreen.js
+++ screens/ReportScreens/ReportPhotoScreen.js
@@ -1,32 +1,115 @@
 import React from 'react';
-import {  StyleSheet, Text, Image, View, ScrollView, Dimensions, TouchableOpacity } from 'react-native';
+import {  StyleSheet, Text, Image, View, ScrollView, Alert, SafeAreaView,
+         ActionSheetIOS, Dimensions, TouchableOpacity, Permissions, Platform } from 'react-native';
 import { AntDesign, Entypo } from '@expo/vector-icons'
 import Header from '../../components/Header';
 import InfoMessage from '../../components/InfoMessage';
 import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
+import * as ImagePicker from 'expo-image-picker';
 
 export default class ReportScreen extends React.Component {
+  state = { images: [], imageCount: 0 }
+
+  componentDidMount() {
+    this.getPermissionAsync();
+  }
+
+  getPermissionAsync = async () => {
+    if (Constants.platform.ios) {
+      const { status } = await Permissions.askAsync(Permissions.CAMERA_ROLL);
+      if (status !== 'granted') {
+        alert('Sorry, we need camera roll permissions to make this work!');
+      }
+    }
+  }
+
+  _pickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      quality: 1,
+      base64: true
+    });
+    if (!result.cancelled) {
+      this.setState((state) => {
+        const images = state.images.concat(result['base64'])
+        return {
+          images: images
+        }
+      });
+    }
+  };
+
+  _takePhoto = async () => {
+    let result = await ImagePicker.launchCameraAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      quality: 1,
+      base64: true
+    });
+    if (!result.cancelled) {
+      this.setState((state) => {
+        const images = state.images.concat(result['base64'])
+        return {
+          images: images
+        }
+      });
+    }
+  }
+
+  photoAlert = () => {
+    if (Platform.OS == 'android') {
+      Alert.alert('Get a photo from...','', [
+        {text: 'Camera', onPress: () => this._takePhoto()},
+        {text: 'Take from Libary', onPress: () => this._pickImage()},
+        {text: 'Cancel', style: 'cancel',}
+      ])
+    } else {
+      ActionSheetIOS.showActionSheetWithOptions(
+        {
+          options: ['Cancel', 'Camera', 'Take from Libaray'],
+          cancelButtonIndex: 0,
+        },
+        (buttonIndex) => {
+          if (buttonIndex === 1) {
+            this._takePhoto()
+          } else if (buttonIndex === 2) {
+            this._pickImage()
+          }
+        },
+      );
+    }
+  }
+
+  removeImage = (index) => {
+    this.setState((state) => {
+      const images = state.images.filter((item, j) => index !== j);
+      return {
+        images: images
+      }
+    })
+  }
 
   createReport = ({ location, details, magisterialDistrict, priority }) => {
     createReport(location, details, magisterialDistrict, priority);
   }
-
+  
   handleSubmit = (values) => {
-    console.log(values);
+     console.log(values);
   }
 
   renderImages = () => {
-    const images = []
-    if (images.length > 0) {
-      return images.forEach(photo => {
+    if (this.state.images.length > 0) {
+      return this.state.images.map((photo, index) => {
         return (
-          <Image source={{ isStatic: true, uri: 'data:image/jpeg;base64,'+ photo.base64, }} style={styles.image}>
-              <View style={styles.imageDeleteContainer}>
-                <TouchableOpacity style={styles.imageDelete}>
+          <View style={styles.imageWrapper}>
+            <Image source={{ isStatic: true, uri: 'data:image/jpeg;base64,'+photo, }} style={styles.image}/>
+            <View style={styles.imageDeleteContainer}>
+                <TouchableOpacity style={styles.imageDelete} onPress={() => this.removeImage(index)}>
                   <AntDesign name="delete" size={25} color={'white'}/>
                 </TouchableOpacity>
               </View>
-          </Image>
+          </View>
         )
       });
     } else {
@@ -43,7 +126,7 @@ export default class ReportScreen extends React.Component {
   render() {
     const { navigation } = this.props
     return (
-        <View style={styles.container}>
+        <SafeAreaView style={styles.container}>
             <Header title="Photos" {...this.props}
               navTitleOne="Home" navTitleTwo="Next"
               navActionOne={() => navigation.navigate('Home')} 
@@ -55,10 +138,10 @@ export default class ReportScreen extends React.Component {
               snapToInterval={310} snapToAlignment={"center"}>
               {this.renderImages()}
             </ScrollView>
-            <TouchableOpacity style={styles.button}>
+            <TouchableOpacity style={styles.button} onPress={this.photoAlert}>
               <Text style={{fontSize: 25, color: 'white'}}>Add Photo</Text>
             </TouchableOpacity>
-        </View>
+        </SafeAreaView>
     );
   }
 }
@@ -76,11 +159,15 @@ const styles = StyleSheet.create({
     marginTop: hp('1%'),
     alignItems: 'center', justifyContent: 'center',
   },
-  image: {
-    height: 400, width: 300,
+  imageWrapper: {
+    height: 400, width: 350,
     backgroundColor: '#333',
     borderRadius: 20, margin: 10,
     position: 'relative'
+  },
+  image: {
+    height: 400, width: 350,
+    borderRadius: 20,
   },
   imageDelete: {
     width: 40, height: 40,
@@ -95,7 +182,7 @@ const styles = StyleSheet.create({
     width: 350, height: 50,
     borderRadius: 42, borderWidth: 2, borderColor: '#E9E9E9',
     justifyContent: 'center', alignItems: 'center',
-    marginBottom: hp('4%')
+    marginBottom: hp('15%')
   },
   imagesPlaceholder: {
     height: 400, width: 350,
