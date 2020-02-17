f3cdbe566dbce2a96b0fd57e6155a2346c45d282
Tanner York <tannerwyork@gmail.com>
2020-02-17 12:17:20 -0800

    Connect profile screen to firebase

--- screens/HomeScreens/ProfileScreen.js
+++ screens/HomeScreens/ProfileScreen.js
@@ -1,12 +1,53 @@
 import React from 'react';
-import { StyleSheet, View, Text, TextInput, TouchableOpacity, SafeAreaView } from 'react-native';
+import {
+  StyleSheet,
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  SafeAreaView,
+  Alert
+} from 'react-native';
 import {
   widthPercentageToDP as wp,
   heightPercentageToDP as hp
 } from 'react-native-responsive-screen';
+import { firebaseApp, signOut } from '../../config/FirebaseApp';
 
 export default class ProfileScreen extends React.Component {
+  componentWillMount() {
+    const { displayName, email, phoneNumber } = firebaseApp.auth().currentUser;
+    this.setState({ name: displayName, email: email, phone: phoneNumber });
+  }
+
+  updateProfile = () => {
+    const { currentUser } = firebaseApp.auth();
+    const { name, email, phone } = this.state;
+    let errors = {};
+    if (!name) {
+      errors.name = 'Name is required';
+    }
+    if (!email) {
+      errors.email = 'Email is required';
+    }
+    if (Object.values(errors).length > 0) {
+      Alert.alert(
+        'A Required Field Is Missing',
+        'Check that your have filled out the name and email, and that you hav eentered a valid email.',
+        [{ text: 'Ok', style: 'cancel' }]
+      );
+    } else {
+      currentUser.updateProfile({
+        displayName: name,
+        email: email,
+        phoneNumber: phone
+      });
+    }
+  };
+
   render() {
+    const { name, email, phone } = this.state;
+    const { navigate } = this.props.navigation;
     return (
       <SafeAreaView style={styles.container}>
         <View style={styles.headerWrapper}>
@@ -15,28 +56,58 @@ export default class ProfileScreen extends React.Component {
         <View style={styles.inputs}>
           <View style={styles.inputWrapper}>
             <Text style={styles.inputLabel}>Name</Text>
-            <TextInput style={styles.input} />
+            <TextInput
+              style={styles.input}
+              value={name}
+              onChangeText={text => this.setState({ name: text })}
+            />
           </View>
           <View style={styles.inputWrapper}>
             <Text style={styles.inputLabel}>Email</Text>
-            <TextInput style={styles.input} />
+            <TextInput
+              style={styles.input}
+              value={email}
+              onChangeText={text => this.setState({ email: text })}
+            />
           </View>
           <View style={styles.inputWrapper}>
             <Text style={styles.inputLabel}>Phone</Text>
-            <TextInput style={styles.input} />
+            <TextInput
+              style={styles.input}
+              value={phone}
+              onChangeText={text => this.setState({ phone: text })}
+            />
           </View>
-          <View style={styles.inputWrapper}>
+          {/* <View style={styles.inputWrapper}>
             <Text style={styles.inputLabel}>Address</Text>
             <TextInput style={styles.input} />
-          </View>
+          </View> */}
         </View>
         <View style={styles.buttons}>
-          <TouchableOpacity style={styles.button} onPress={this.photoAlert}>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => {
+              Alert.alert(
+                'Are you sure you want to make these changes?',
+                'Changes can be made latter.',
+                [
+                  { text: 'Cancel', style: 'cancel' },
+                  { text: 'Ok', onPress: () => this.updateProfile() }
+                ]
+              );
+            }}
+          >
             <Text style={{ fontSize: wp('6%'), color: 'white', fontWeight: 'bold' }}>
               Save Changes
             </Text>
           </TouchableOpacity>
-          <TouchableOpacity style={styles.button} onPress={this.photoAlert}>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => {
+              signOut();
+              navigate('SignIn');
+            }}
+          >
             <Text style={{ fontSize: wp('6%'), color: 'white', fontWeight: 'bold' }}>Logout</Text>
           </TouchableOpacity>
         </View>
